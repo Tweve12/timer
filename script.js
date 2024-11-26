e5c5ea99b78f7a53f0dfd5015eaca075f264bcda
Tweve12 <vmakarovskaa3@gmail.com>
2024-11-26 20:33:16 +0300

    add min and sec in function updateTimer

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 let start = document.querySelector(".button__start");
 let stop = document.querySelector(".button__stop");
 let hoursTime = document.querySelector("#hours");
-let minutsTime = document.querySelector("#minuts");
+let minutesTime = document.querySelector("#minutes");
 let secondsTime = document.querySelector("#seconds");
+let hour;
+let min;
+let sec;
+let stayTime;
 
 function startTimer(){
-    let hour = +hoursTime.value;
-    let min = +hoursTime.value;
-    let sec = +hoursTime.value;
-    let allSec = hour*3600+min*60+sec;
-    console.log(allSec);
-
+    hour = +hoursTime.value;
+    min = +minutesTime.value;
+    sec = +secondsTime.value;
+    stayTime = (hour*3600)+(min*60)+sec;
+    setInterval(updateTimer, 1000);
 };
 
 function updateTimer(){
+    stayTime -= 1;
 
+    hour = stayTime/3600;
+    min = stayTime%3600/60
+    sec = stayTime%60
+    
+
+
+    hoursTime.value = Math.floor(hour);
+    minutesTime.value = Math.floor(min);
+    secondsTime.value = Math.floor(sec);
 };
 
 function stopTimer(){
