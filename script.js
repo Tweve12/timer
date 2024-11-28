68b1cdbd49eb6416700d26b5e5e560c3d5516701
Tweve12 <vmakarovskaa3@gmail.com>
2024-11-28 19:39:19 +0300

    add min and sec in function updateTimer

--- script.js
+++ script.js
@@ -7,31 +7,55 @@ let hour;
 let min;
 let sec;
 let stayTime;
+let interval;
 
 function startTimer(){
     hour = +hoursTime.value;
     min = +minutesTime.value;
     sec = +secondsTime.value;
     stayTime = (hour*3600)+(min*60)+sec;
-    setInterval(updateTimer, 1000);
+
+    start.classList.add("hide");
+    stop.classList.add("show");
+
+    start.classList.remove("show");
+    stop.classList.remove("hide");
+
+    document.documentElement.requestFullscreen();
+
+    interval = setInterval(updateTimer, 1000);
 };
 
 function updateTimer(){
     stayTime -= 1;
 
-    hour = stayTime/3600;
-    min = stayTime%3600/60
-    sec = stayTime%60
+    hour = Math.floor(stayTime/3600);
+    min = Math.floor(stayTime%3600/60);
+    sec = Math.floor(stayTime%60);
     
-
-
-    hoursTime.value = Math.floor(hour);
-    minutesTime.value = Math.floor(min);
-    secondsTime.value = Math.floor(sec);
+    hour = hour.toString().padStart(2, "0");
+    min = min.toString().padStart(2, "0");
+    sec = sec.toString().padStart(2, "0");
+    
+    
+    hoursTime.value = hour;
+    minutesTime.value = min;
+    secondsTime.value = sec;
 };
 
 function stopTimer(){
-    console.log(2);
+    stop.classList.add("hide");
+    start.classList.add("show");
+
+    stop.classList.remove("show");
+    start.classList.remove("hide");
+
+    setTimeout(() => {
+        document.exitFullscreen();
+    }, 1000);
+    
+
+    clearInterval(interval);
 };
 
 start.addEventListener("click", startTimer);
